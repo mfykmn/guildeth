68ab49446dd669e8cb637075d828ef0d78d2b137
mafuyuk <mafuyuk.m@gmail.com>
2018-06-11 23:39:17 +0900

    Use contract

--- front/index.js
+++ front/index.js
@@ -9,3 +9,24 @@ web3.eth.accounts.forEach((account) => {
 console.log(account);
 console.log(balance);
 });
+
+//コントラクトのアドレス
+const address = "0xcdC1Cb1bFcEdAC94Cb7771c9AD427B2D044aBF96";
+
+//abi情報
+const abi = [{"constant": false, "inputs": [{"name": "_message", "type": "string"}], "name": "set", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function"}, {"constant": true, "inputs": [], "name": "get", "outputs": [{"name": "_data", "type": "string"}],"payable": false,"stateMutability": "view","type": "function"}];
+
+//実行
+contract(address, abi).then((c) => {
+  c.set.call("aaaa");
+  return c;
+}).then((c) => {
+  return c.get.call();
+}).then((res) => {
+  console.log("response:", res);
+});
+
+//コントラクトの取得
+async function contract(address, abi) {
+  return await web3.eth.contract(abi).at(address);
+}
