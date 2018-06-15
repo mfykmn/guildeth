ff0e0946eb76107509b4b6153fa7e6eb86f50de4
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-06-15 16:23:15 +0900

    Removed .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,37 @@
 const path = require('path');
 
 module.exports = {
-    entry: [
-        './src/quests.js'
-    ],
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            { test: /\.js$/, use: 'babel-loader' },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    {loader: 'css-loader', options: {url: false}},
-                ],
-            }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        port: 9000
-    },
-    performance: { hints: false }
+  entry: [
+    './src/quests.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {loader: 'css-loader', options: {url: false}},
+        ],
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    port: 9000
+  },
+  performance: { hints: false }
 };
