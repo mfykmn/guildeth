96dcb21606473ba4120345b704b31956665d8337
mafuyuk <mafuyuk.m@gmail.com>
2018-06-16 22:37:35 +0900

    Use props

--- src/js/components/quests.js
+++ src/js/components/quests.js
@@ -2,25 +2,36 @@ import React, {Fragment}  from "react";
 
 import "../../css/quests.css";
 
-const Quest = () => {
+const Quest = (props) => {
   return (
     <div className="quest">
-      <h3>〇〇をしてほしい</h3>
-      <p>対応期限: 2018-01-01T00:00:00+09:00</p>
-      <p>報酬　　: 0.54eth</p>
+      <h3>{props.title}</h3>
+      <p>内容　　 : {props.text}</p>
+      <p>対応期限 : {props.deadline}</p>
+      <p>報酬　　 : {props.amount}eth</p>
     </div>
   );
 };
 
-const Quests = () => {
+const Quests = (props) => {
+  const listQuests = props.data.map((data) => {
     return (
-        <Fragment>
-          <h3 className="section-title">クエスト一覧</h3>
-          <Quest/>
-          <Quest/>
-          <Quest/>
-        </Fragment>
+      <Quest
+        key={data.id}
+        title={data.title}
+        text={data.text}
+        deadline={data.deadline}
+        amount={data.amount}
+      />
     );
+  });
+
+  return (
+      <Fragment>
+        <h3 className="section-title">クエスト一覧</h3>
+        {listQuests}
+      </Fragment>
+  );
 };
 
 export default Quests;
--- src/root.js
+++ src/root.js
@@ -7,12 +7,19 @@ import Quests from './js/components/quests'
 
 import './css/index.css';
 
+const questListData = [
+  { id: 0, title: "〇〇1をしてほしい", text: "〇〇の〇〇が発生しています。対処してください。", deadline: "2018-01-01T00:00:00+09:00", amount: 0.54},
+  { id: 1, title: "〇〇2をしてほしい", text: "〇〇の〇〇が発生しています。対処してください。", deadline: "2018-05-01T00:00:00+09:00", amount: 1},
+  { id: 2, title: "〇〇3をしてほしい", text: "〇〇の〇〇が発生しています。対処してください。", deadline: "2018-04-01T00:00:00+09:00", amount: 0.02},
+  { id: 3, title: "〇〇4をしてほしい", text: "〇〇の〇〇が発生しています。対処してください。", deadline: "2018-03-01T00:00:00+09:00", amount: 0.9},
+];
+
 const Root = () => {
   return (
     <Fragment>
       <Heaser />
       <Body />
-      <Quests />
+      <Quests data={questListData}/>
     </Fragment>
   );
 
